71daae78a80cc62bb8ce1296e5ef0fb0c9dc41bb
li <im.yuqlee@gmail.com>
2026-05-12 15:10:11 +0800

    Fix load html

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import manifest from './manifest.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: 'localhost',
+      port: 5173,
+    },
+  },
   plugins: [
     react(),
     crx({ manifest }),
